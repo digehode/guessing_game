c114c8e7646f3d2fd20e1c0da52c803be9bb2e9c
digehode <digehode@gmail.com>
2022-11-09 11:43:05 -0800

    Improved the user user experience

--- game.py
+++ game.py
@@ -9,14 +9,17 @@ answer=random.randint(minimum,maximum)
 
 guess=None
 
+print("You will be asked to guess a number. Try to do it in as few attempts as possible")
+
 while guess!=answer:
-    guess=int(input("Enter a guess:"))
+    guess=int(input("Enter a guess: "))
     
     if guess==answer:
-        print("Well done")
+        print("Well done.")
     elif guess>answer:
         print("Too high!")
     else:
         print("Too low!")
-print("Game over")    
+        
+print("Thank you for playing!")    
   
